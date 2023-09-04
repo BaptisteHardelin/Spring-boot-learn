16d4ef0596840b90e20414708b60699a4e8e849d
BaptisteHardelin <baptiste.hardelin@gmail.com>
2023-09-04 15:53:12 +0200

    init + simple hello world

---
+++ src/test/java/fr/babaprog/spring/boot/tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.babaprog.spring.boot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
